94011cb0b428f7a2056c4a4d420cec04d5f8d9d3
Irem Ugurlu <iugurlu@bol.com>
2022-11-24 20:16:22 +0100

    Add basic setup and commands

---
+++ database.py
@@ -0,0 +1,108 @@
+import psycopg2
+
+def connect():
+    try:
+        try:
+            # try to connect to database
+            conn = psycopg2.connect(
+                host="localhost",
+                database="bookstore",
+                user="postgres",
+                password="postgres")
+        except (Exception, psycopg2.DatabaseError) as error:
+            # if database doesn't exist, create it
+            conn = psycopg2.connect(
+                host="localhost",
+                database="bookstore",
+                user="postgres",
+                password="postgres")
+            conn.autocommit = True
+            curr = conn.cursor()
+            curr.execute("CREATE DATABASE bookstore")
+            curr.close() 
+            conn = psycopg2.connect(
+                host="localhost",
+                database="bookstore",
+                user="postgres",
+                password="postgres")
+
+        conn.autocommit = True
+        curr = conn.cursor()
+        # check which tables we have
+        curr.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+        table_names = curr.fetchall()
+        # if there are no tables create them
+        if len(table_names) == 0:
+            file = open('bookstore.sql', 'r')
+            sqlFile = file.read()
+            file.close()
+            # all SQL commands (split on ';')
+            sqlCommands = sqlFile.split(';')[:-1]
+            # Execute every command from the input file
+            for command in sqlCommands:
+                try:
+                    curr.execute(command)
+                except (Exception, psycopg2.DatabaseError) as error:
+                    break
+            curr.close()  
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)      
+    finally: 
+        if conn is not None:
+            conn.close()
+
+def get_users():
+    conn = psycopg2.connect(
+            host="localhost",
+            database="bookstore",
+            user="postgres",
+            password="postgres")
+    conn.autocommit = True
+    curr = conn.cursor()
+    curr.execute('SELECT username FROM public."Users"')
+    users = curr.fetchall()
+    usernames = []
+    for user in users:
+        usernames.append(user[0])
+    curr.close()
+    conn.close()
+    return usernames
+
+def add_user(user):
+    conn = psycopg2.connect(
+            host="localhost",
+            database="bookstore",
+            user="postgres",
+            password="postgres")
+    conn.autocommit = True
+    curr = conn.cursor()
+    curr.execute(f'INSERT INTO public."Users"(username) VALUES (\'{user}\')')
+    curr.close()
+    conn.close()
+    
+def get_books():
+    conn = psycopg2.connect(
+            host="localhost",
+            database="bookstore",
+            user="postgres",
+            password="postgres")
+    conn.autocommit = True
+    curr = conn.cursor()
+    curr.execute('SELECT * FROM public."Book"')
+    books = curr.fetchall()
+    curr.close()
+    conn.close()
+    return books
+
+def add_book(name, author, page, genre):
+    conn = psycopg2.connect(
+            host="localhost",
+            database="bookstore",
+            user="postgres",
+            password="postgres")
+    conn.autocommit = True
+    curr = conn.cursor()
+    curr.execute(f'INSERT INTO public."Book"(name, author, page, genre) VALUES (\'{name}\', \'{author}\', {int(page)}, \'{genre}\')')
+    curr.close()
+    conn.close()
---
+++ main.py
@@ -0,0 +1,71 @@
+import typer
+from database import connect, get_users, add_user, get_books, add_book
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+app = typer.Typer()
+
+@app.command("start")
+def start():
+    typer.echo(f"Welcome to Book Store!")
+    typer.echo(f"You can execute command 'help' to see possible commands")
+    connect()
+
+@app.command("help")
+def help():
+    typer.echo(f"Here are the possible commands:")
+    for command in app.registered_commands:
+        print(command.name)
+
+@app.command("signup")
+def signup():
+    typer.echo(f"Nice that you are signing up!")
+    username = input("Please enter your username:")
+    users = get_users()
+    while username in users:
+        print("This username already exists!")
+        username = input("Please enter another username:")
+    add_user(username)
+    print("Successfully signed up!")
+
+@app.command("signin")
+def signin():
+    typer.echo(f"Let's sign in!")
+    name = input("Please enter your username:")
+    users = get_users()
+    if name in users:
+        print("This username doesn't exist. Please sign up!")
+        return
+
+@app.command("addbook")
+def addbook():
+    typer.echo(f"Please enter the required book info to add!")
+    name = input("Book name: ")
+    author = input("Author: ")
+    page = input("Number of pages :")
+    genre = input("Genre of the book: ")
+    add_book(name, author, page, genre)
+    typer.echo(f"Book successfully added!")
+
+@app.command("books")
+def books():
+    books = get_books()
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("#", style="dim", width=10)
+    table.add_column("Book ID", style="dim", min_width=10, justify=True)
+    table.add_column("Book", style="dim", min_width=10, justify=True)
+    table.add_column("Author", style="dim", min_width=10, justify=True)
+    table.add_column("Pages", style="dim", min_width=10, justify=True)
+    table.add_column("Genre", style="dim", min_width=10, justify=True)
+
+    for idx, book in enumerate(books, start=1):
+        print(book)
+        table.add_row(str(idx), str(book[0]), book[1], book[2], str(book[3]), book[4])
+
+    console.print(table)
+    
+
+if __name__ == "__main__":
+    app()
